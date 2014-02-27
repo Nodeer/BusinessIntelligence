881155d90d1fafb86615da79de689b8b3e529a2b
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-02-27 06:21:05 +0300

    modules

---
+++ routes/tasks.js
@@ -0,0 +1,17 @@
+﻿exports.register = function (app, passport) {
+
+    var auth = require('../services/auth');
+
+    app.get('/tasks.json', auth.authenticate, this.tasksJson);
+    app.get('/tasks/:taskId.json', auth.authenticate, this.tasksJson);
+};
+
+exports.tasksJson = function (req, res, next) {
+    if (req.params.taskId) {
+    } else {
+        var db = require('../repository/db');
+        db.procedure('usp_GetTasks', [], function (recordset) {
+            res.json(recordset[0]);
+        });
+    }
+};
---
+++ settings.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    db: {
+        log: true,
+        config: {
+            driver: 'msnodesql',
+            connectionString: 'Driver={SQL Server Native Client 11.0};Server=tcp:BABITSKY,1433;Database=BusinessIntelligence;Uid=bi;Pwd=dajklhjklmdas02'
+        }
+    },
+    hash: {
+        salt: 'sdajklskaisd9ia09jdskadosakodk32op3o40-rifdsjf;dks;sdkidsu43poi2eoidsjklsakdsai9302'
+    }
+};
