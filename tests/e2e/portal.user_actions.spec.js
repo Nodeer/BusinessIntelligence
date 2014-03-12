b84b599e16cccad12446b21a1b1febd2c33ccfab
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-03-12 07:36:07 +0300

    Tests update

--- tests/e2e/portal.user_actions.spec.js
+++ tests/e2e/portal.user_actions.spec.js
@@ -1,60 +1,98 @@
-﻿describe('an user', function() {
+﻿var Application = function() {
+    this.profile = {
+        username: element(by.name('username')),
+        password: element(by.name('password')),
+        signin: element(by.id('btn-signin')),
+        signup: element(by.id('btn-signup')),
+        signout: element(by.id('btn-signout'))
+    };
+
+    this.alerts = {
+        danger: $('div .alert.alert-danger').$('p')
+    };
+};
+
+var User = function(app) {
+    this.app = app;
+
+    this.signin = function(username, password) {
+        ///<summary>Sign in</summary>
+        ///<param name="username">Username</param>
+        ///<param name="password">Password</param>
+        
+        this.app.profile.username.sendKeys(username);
+        this.app.profile.password.sendKeys(password);
+        this.app.profile.signin.sendKeys(protractor.Key.ENTER);
+    };
+
+    this.signup = function(username, password) {
+        ///<summary>Sign up</summary>
+        ///<param name="username">Username</param>
+        ///<param name="password">Password</param>
+
+        this.app.profile.username.sendKeys(username);
+        this.app.profile.password.sendKeys(password);
+        this.app.profile.signup.sendKeys(protractor.Key.ENTER);
+    };
+
+    this.signout = function() {
+        ///<summary>Sign out</summary>
+        
+        this.app.profile.signout.sendKeys(protractor.Key.ENTER);
+    };
+};
+
+describe('an user', function() {
     browser.get('/');
 
-    it('should see error message if he used an incorrect login', function() {
-        element(by.name('username')).sendKeys('incorrectLogin');
-        element(by.name('password')).sendKeys('incorrectPassword');
-        element(by.id('btn-signin')).sendKeys(protractor.Key.ENTER);
+    var app = new Application();
+    var user = new User(app);
 
-        expect(element(by.id('btn-signin')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeFalsy();
-        expect(element(by.css('div .alert.alert-danger > p')).getText()).toBe('Login failed.');
+    it('should see error message if he used an incorrect login', function() {
+        user.signin('badlogin', 'badpassword');
+
+        expect(app.profile.signin.isPresent()).toBeTruthy();
+        expect(app.profile.signout.isPresent()).toBeFalsy();
+        expect(app.alerts.danger.getText()).toBe('Login failed.');
     });
 
     it('should see an error message if login is already used during signup', function() {
-        element(by.name('username')).sendKeys('asd');
-        element(by.name('password')).sendKeys('doesnotmatter');
-        element(by.id('btn-signup')).sendKeys(protractor.Key.ENTER);
+        user.signup('asd', 'doesnotmatter');
 
-        expect(element(by.id('btn-signin')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeFalsy();
-        expect(element(by.css('div .alert.alert-danger > p')).getText()).toBe('The user is already exists.');
+        expect(app.profile.signin.isPresent()).toBeTruthy();
+        expect(app.profile.signout.isPresent()).toBeFalsy();
+        expect(app.alerts.danger.getText()).toBe('The user is already exists.');
     });
 
     it('should see able to sign in with valid username / password', function() {
-        element(by.name('username')).sendKeys('asd');
-        element(by.name('password')).sendKeys('asd');
-        element(by.id('btn-signin')).sendKeys(protractor.Key.ENTER);
+        user.signin('asd', 'asd');
 
-        expect(element(by.id('btn-signin')).isPresent()).toBeFalsy();
-        expect(element(by.id('btn-signup')).isPresent()).toBeFalsy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeTruthy();
+        expect(app.profile.signin.isPresent()).toBeFalsy();
+        expect(app.profile.signup.isPresent()).toBeFalsy();
+        expect(app.profile.signout.isPresent()).toBeTruthy();
     });
 
     it('should be able to sign out after sign in', function() {
-        element(by.id('btn-signout')).sendKeys(protractor.Key.ENTER);
+        user.signout();
 
-        expect(element(by.id('btn-signin')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signup')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeFalsy();
+        expect(app.profile.signin.isPresent()).toBeTruthy();
+        expect(app.profile.signup.isPresent()).toBeTruthy();
+        expect(app.profile.signout.isPresent()).toBeFalsy();
     });
 
     it('should see able to sign up', function() {
-        element(by.name('username')).sendKeys('testusername' + (new Date().getTime() / 1000));
-        element(by.name('password')).sendKeys('testpassword');
+        user.signup('testusername' + (new Date().getTime()), 'testpassword');
 
-        element(by.id('btn-signup')).sendKeys(protractor.Key.ENTER);
-
-        expect(element(by.id('btn-signin')).isPresent()).toBeFalsy();
-        expect(element(by.id('btn-signup')).isPresent()).toBeFalsy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeTruthy();
+        expect(app.profile.signin.isPresent()).toBeFalsy();
+        expect(app.profile.signup.isPresent()).toBeFalsy();
+        expect(app.profile.signout.isPresent()).toBeTruthy();
     });
 
     it('should be able to sign out after sign up', function() {
-        element(by.id('btn-signout')).sendKeys(protractor.Key.ENTER);
+        user.signout();
 
-        expect(element(by.id('btn-signin')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signup')).isPresent()).toBeTruthy();
-        expect(element(by.id('btn-signout')).isPresent()).toBeFalsy();
+        expect(app.profile.signin.isPresent()).toBeTruthy();
+        expect(app.profile.signup.isPresent()).toBeTruthy();
+        expect(app.profile.signout.isPresent()).toBeFalsy();
     });
 });
