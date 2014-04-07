c6dbd3df6ea174d6473b79d2958d2974cb9da0f0
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-04-07 16:19:18 +0300

    Gruntfile update

--- gruntfile.js
+++ gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       files: ['Gruntfile.js',
-			  './public/**/app.js',
-			  './public/**/controllers.js',
-			  './public/**/services.js',
-			  './models/**/*.js',
-			  './repository/**/*.js',
-			  './routes/**/*.js',
-			  './services/**/*.js',
-			  './tests/**/*.spec.js'
+              './public/**/app.js',
+              './public/**/controllers.js',
+              './public/**/services.js',
+              './models/**/*.js',
+              './repository/**/*.js',
+              './routes/**/*.js',
+              './services/**/*.js',
+              './tests/**/*.spec.js'
         ],
       options: {
         globals: {
@@ -23,14 +23,23 @@ module.exports = function(grunt) {
         }
       }
     },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    }
+    protractor: {
+        options: {
+          keepAlive: true, // If false, the grunt process stops when the test fails.
+          noColor: false // If true, protractor will not use colors in its output.
+        },
+        tests_e2e: {
+          options: {
+            configFile: "tests.e2e.js", // Target-specific config file
+            args: {} // Target-specific arguments
+          }
+        },
+      }
   });
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-protractor-runner');
   
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'protractor:tests_e2e']);
 
 };
