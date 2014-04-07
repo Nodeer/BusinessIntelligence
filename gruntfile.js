f2345860a105002c2d4b0635abccf004b39e27c7
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-04-07 17:26:24 +0300

    GruntFile

--- gruntfile.js
+++ gruntfile.js
@@ -40,6 +40,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-protractor-runner');
   
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint'] );
 
 };
